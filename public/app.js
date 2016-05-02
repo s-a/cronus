346ba69f9c226505daddf7471b9c4e9322822624
s-a <stephan.ahlf@googlemail.com>
2016-05-02 22:18:10 +0200

    :bee:

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@
 (function(io){
 	var url = location.protocol + "//" + location.hostname + (location.port ? ":" + location.port : "");
 	var socket = io.connect(url);
+	var x;
 	socket.on("init", function (data) {
 		var s = [];
 		console.warn(data);
@@ -10,23 +11,10 @@
 		}
 
 		console.log(s);
-		//x.setState( {items : s} );
+		render(s);
 		document.getElementById("state").innerHTML = data.hello;
-
-		var x = ReactDOM.render(
-		    <ServiceChooser items={ s } />,
-		    document.getElementById('container')
-		);
 		//socket.emit("my other event", { my: "data" });
 	});
-
-
-
-
-
-	// This is more complex example that uses two components -
-	// a service chooser form, and the individual services inside it.
-
 
 	var ServiceChooser = React.createClass({
 
@@ -47,19 +35,15 @@
 	            // Create a new Service component for each item in the items array.
 	            // Notice that I pass the self.addTotal function to the component.
 
-	            return <Service name={s.name} price={s.price} active={s.active} addTotal={self.addTotal} />;
+	            return <Service name={s.name} description={s.description} price={s.price} active={s.active} addTotal={self.addTotal} />;
 	        });
 
 	        return <div>
 	                    <h1>Our services</h1>
-	                    
 	                    <div id="services">
 	                        {services}
-
 	                        <p id="total">Total <b></b></p>
-
 	                    </div>
-
 	                </div>;
 
 	    }
@@ -67,42 +51,29 @@
 
 
 	var Service = React.createClass({
-
 	    getInitialState: function(){
 	        return { active: false };
 	    },
-
 	    clickHandler: function (){
-
 	        var active = !this.state.active;
-
 	        this.setState({ active: active });
-	        
 	        // Notify the ServiceChooser, by calling its addTotal method
 	        this.props.addTotal( active ? this.props.price : -this.props.price );
-
 	    },
-
 	    render: function(){
-
 	        return  <p className={ this.state.active ? 'active' : '' } onClick={this.clickHandler}>
-	                    {this.props.name} <b></b>
+	                    {this.props.description} <b>{this.props.name}</b>
 	                </p>;
-
 	    }
-
 	});
 
 
-	var services = [
-	    { name: 'Web Development', price: 300 },
-	    { name: 'Design', price: 400 },
-	    { name: 'Integration', price: 250 },
-	    { name: 'Training', price: 220 }
-	];
 
-
-	// Render the ServiceChooser component, and pass the array of services
-
+	var render = function (data) {
+		x = ReactDOM.render(
+		    <ServiceChooser items={ data } />,
+		    document.getElementById('container')
+		);
+	}
 
 })(io);
