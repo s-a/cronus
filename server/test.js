db130656e94ed191bc3c2ce79f48c0ad766a47aa
s-a <stephan.ahlf@googlemail.com>
2016-05-03 19:37:21 +0200

    add test.js

---
+++ server/test.js
@@ -0,0 +1,14 @@
+"use strict"
+
+var path = require("path");
+var minimist = require("minimist");
+var argv = minimist(process.argv.slice(2));
+
+if (argv.script){
+	var JOB = require(path.resolve(argv.script));
+	var job = new JOB();
+	console.log("start");
+	var result = job.test();
+	console.log("result", result);
+	console.log("done");
+}
