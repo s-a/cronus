7705e8e19c7c4673d097a7879396b1c37725157e
s-a <stephan.ahlf@gmail.com>
2022-10-17 07:02:16 +0200

    upgrade dependecies

--- server/app.js
+++ server/app.js
@@ -15,14 +15,14 @@ var port = argv.port || 3000;
 server.listen(port);
 
 app.use(express.static("public"));
-app.use(express.static("node_modules/socket.io/node_modules/socket.io-client/"));
+app.use(express.static("node_modules/socket.io/client-dist/"));
 
 
 var JobController = require("./job-controller");
 var jobController = new JobController(io);
-var pack  = require("./../package.json");
+var pack = require("./../package.json");
 var serverStartTime = new Date().getTime();
- 
+
 io.on("connection", function (socket) {
 	socket.emit("init", { crons: jobController.jobs, package: pack, serverStartTime: serverStartTime });
 	/*socket.on("my other event", function (data) {
